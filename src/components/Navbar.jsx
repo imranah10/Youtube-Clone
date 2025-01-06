5b3e52e378c6ca9991e1a003cd57c81233b91a62
imran <imranaha310@gmail.com>
2025-01-06 20:16:17 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
+import { ImYoutube2 } from "react-icons/im";
+const Navbar = () => {
+  return (
+    <div className='flex justify-between px-4 py2'>
+      <div className='border border-red-500'>
+      <AiOutlineMenu />
+      
+      </div>
+      <div className='border border-red-500'>2</div>
+      <div className='border border-red-500'>3</div>
+    </div>
+  )
+}
+
+export default Navbar
